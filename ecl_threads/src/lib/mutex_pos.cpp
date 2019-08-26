2a86052617b451bcdca5fcff4e99651e73fc8b49
Daniel Stonier <d.stonier@gmail.com>
2019-08-25 22:30:39 -0400

    Rebase from 'upstream'

--- ecl_threads/src/lib/mutex_pos.cpp
+++ ecl_threads/src/lib/mutex_pos.cpp
@@ -58,7 +58,6 @@ Mutex::Mutex(const bool locked) :
     this->lock();
   }
 }
-;
 
 Mutex::~Mutex()
 {
@@ -73,7 +72,6 @@ void Mutex::lock()
   int result = pthread_mutex_lock(&mutex);
   ecl_assert_throw(result == 0, threads::throwMutexLockException(LOC,result));
 }
-;
 
 bool Mutex::trylock(Duration &duration)
 {
@@ -88,11 +86,11 @@ bool Mutex::trylock(Duration &duration)
     ecl_assert_throw(result == 0, threads::throwMutexTimedLockException(LOC,result));
     ++number_locks;
   #else
+    (void) duration
     return trylock(); // fallback option
   #endif
   return true;
 }
-;
 
 bool Mutex::trylock()
 {
@@ -109,7 +107,6 @@ bool Mutex::trylock()
   ++number_locks;
   return true;
 }
-;
 
 void Mutex::unlock()
 {
@@ -117,10 +114,7 @@ void Mutex::unlock()
   int result = pthread_mutex_unlock(&mutex);
   ecl_assert_throw(result == 0, threads::throwMutexUnLockException(LOC,result));
 }
-;
 
-}
-;
-// namespace ecl
+} // namespace ecl
 
 #endif /* ECL_IS_POSIX */
